1e9e13bc5710915d1a10f0e01b391166219ac8b2
M4rkko <Markkogaming@gmail.com>
2026-01-25 22:42:30 +0200

    Car domain

---
+++ Car.Core/Domain/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.Core.Domain
+{
+    public class Car
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
